2726faa6aed8e737cd39ca3abe396f89e794454d
schat22 <siddhartha.chatterjee@albertsons.com>
2024-04-24 13:45:08 +0530

    [Siddhartha] Update github actions and tests

--- src/hotel/hotel.service.ts
+++ src/hotel/hotel.service.ts
@@ -32,30 +32,13 @@ export class HotelService implements OnModuleInit {
           >,
         );
 
-        console.log({
-          findHotelData,
-          user: {
-            username: process.env.COLD_START_USER,
-            password: process.env.COLD_START_PASSWORD,
-            email: process.env.COLD_START_EMAIL,
-            role: process.env.COLD_START_ROLE,
-            service: process.env.COLD_START_SERVICE,
-            isAdmin: process.env.COLD_START_ROLE === 'admin',
-          },
-        });
-
         if (findHotelData.length !== 0) {
           return;
         }
 
-        console.log({
-          URL: 'http://' + process.env.DATA_MAPPING_URI + ':3005/hotel',
-        });
-
         const coldStartHotelList = (await this.axiosService.retryAxiosGet(
           'http://' + process.env.DATA_MAPPING_URI + ':3005/hotel',
         )) as Hotel[];
-        console.log({ coldStartHotelList });
         const superuser = {
           username: process.env.COLD_START_USER,
           password: process.env.COLD_START_PASSWORD,
@@ -65,7 +48,6 @@ export class HotelService implements OnModuleInit {
           isAdmin: process.env.COLD_START_ROLE === 'admin',
           hotels: coldStartHotelList,
         };
-        console.log({ superuser, coldStartHotelList });
         await firstValueFrom(
           this.operatorServiceClient.send('coldStart', superuser).pipe(
             map(async (response: any) => {
@@ -84,7 +66,8 @@ export class HotelService implements OnModuleInit {
           ),
         );
       } catch (error) {
-        throw new error('Cold start failed', 500, error);
+        console.error('Cold start failed:', error);
+        throw new HttpException('Cold start failed', 500);
       }
     }
   }
